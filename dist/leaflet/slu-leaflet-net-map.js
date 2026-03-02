b65fd424a56706e35b13e86c8540c75a95f63c0c
wuqj <137784849@qq.com>
2026-03-02 11:31:17 +0800

    fix：测试

---
+++ dist/leaflet/slu-leaflet-net-map.js
@@ -0,0 +1,204 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SLCRS = exports.SLULeafletNetMap = exports.SLEMap = void 0;
+const L = require("leaflet");
+require("proj4leaflet");
+const slu_map_1 = require("../utils/slu-map");
+'use strict';
+(function (window, document) {
+    L.GridLayer.include({
+        _setZoomTransform: function (level, _center, zoom) {
+            var center = _center;
+            if (center != undefined && this.options) {
+                if (this.options.corrdType == 'gcj02') {
+                    center = (0, slu_map_1.u_mapTogps84gcj02)(_center.lng, _center.lat);
+                }
+                else if (this.options.corrdType == 'bd09') {
+                    center = (0, slu_map_1.u_mapTogps84bd09)(_center.lng, _center.lat);
+                }
+            }
+            var scale = this._map.getZoomScale(zoom, level.zoom), translate = level.origin.multiplyBy(scale)
+                .subtract(this._map._getNewPixelOrigin(center, zoom)).round();
+            if (L.Browser.any3d) {
+                L.DomUtil.setTransform(level.el, translate, scale);
+            }
+            else {
+                L.DomUtil.setPosition(level.el, translate);
+            }
+        },
+        _getTiledPixelBounds: function (_center) {
+            var center = _center;
+            if (center != undefined && this.options) {
+                if (this.options.corrdType == 'gcj02') {
+                    center = (0, slu_map_1.u_mapTogps84gcj02)(_center.lng, _center.lat);
+                }
+                else if (this.options.corrdType == 'bd09') {
+                    center = (0, slu_map_1.u_mapTogps84bd09)(_center.lng, _center.lat);
+                }
+            }
+            var map = this._map, mapZoom = map._animatingZoom ? Math.max(map._animateToZoom, map.getZoom()) : map.getZoom(), scale = map.getZoomScale(mapZoom, this._tileZoom), pixelCenter = map.project(center, this._tileZoom).floor(), halfSize = map.getSize().divideBy(scale * 2);
+            return new L.Bounds(pixelCenter.subtract(halfSize), pixelCenter.add(halfSize));
+        }
+    });
+}(this, document));
+var SLEMap;
+(function (SLEMap) {
+    SLEMap["tianDiTuNormalMap"] = "TianDiTu.Normal.Map";
+    SLEMap["tianDiTuNormalAnnotion"] = "TianDiTu.Normal.Annotion";
+    SLEMap["tianDiTuSatelliteMap"] = "TianDiTu.Satellite.Map";
+    SLEMap["tianDiTuSatelliteAnnotion"] = "TianDiTu.Satellite.Annotion";
+    SLEMap["tianDiTuTerrainMap"] = "TianDiTu.Terrain.Map";
+    SLEMap["tianDiTuTerrainAnnotion"] = "TianDiTu.Terrain.Annotion";
+    SLEMap["gaoDeNormalMap"] = "GaoDe.Normal.Map";
+    SLEMap["gaoDeSatelliteMap"] = "GaoDe.Satellite.Map";
+    SLEMap["gaoDeSatelliteAnnotion"] = "GaoDe.Satellite.Annotion";
+    SLEMap["baiDuNormalMap"] = "Baidu.Normal.Map";
+    SLEMap["baiDuSatelliteMap"] = "Baidu.Satellite.Map";
+    SLEMap["baiDuSatelliteAnnotion"] = "Baidu.Satellite.Annotion";
+    SLEMap["googleNormalMap"] = "Google.Normal.Map";
+    SLEMap["googleSatelliteMap"] = "Google.Satellite.Map";
+    SLEMap["googleSatelliteAnnotion"] = "Google.Satellite.Annotion";
+    SLEMap["geoqNormalMap"] = "Geoq.Normal.Map";
+    SLEMap["geoqNormalPurplishBlue"] = "Geoq.Normal.PurplishBlue";
+    SLEMap["geoqNormalGray"] = "Geoq.Normal.Gray";
+    SLEMap["geoqNormalWarm"] = "Geoq.Normal.Warm";
+    SLEMap["geoqThemeHydro"] = "Geoq.Theme.Hydro";
+    SLEMap["oSMNormalMap"] = "OSM.Normal.Map";
+})(SLEMap || (exports.SLEMap = SLEMap = {}));
+class SLULeafletNetMap {
+    constructor(name, options) {
+        this.setMapProvider(name, options);
+    }
+    addTo(map) {
+        if (!map)
+            return this;
+        this.map = map;
+        this.mapLayer?.addTo(this.map);
+        return this;
+    }
+    remove() {
+        this.mapLayer?.remove();
+        return this;
+    }
+    changeMap(name, options) {
+        this.remove();
+        this.setMapProvider(name, options);
+        this.addTo(this.map);
+        return this;
+    }
+    setMapProvider(name, options) {
+        options = options || {};
+        let parts = name.split('.'), mapSource = parts[0], mapName = parts[1], mapType = parts[2];
+        let url = MAPINFO[mapSource][mapName][mapType];
+        options.subdomains = MAPINFO[mapSource].Subdomains;
+        options.key = options.key || MAPINFO[mapSource].key;
+        options.corrdType = this.getCorrdType(mapSource);
+        if ('tms' in MAPINFO[mapSource]) {
+            options.tms = MAPINFO[mapSource]['tms'];
+        }
+        this.mapLayer = new L.TileLayer(url, options);
+    }
+    getCorrdType(name) {
+        var zbName = "wgs84";
+        switch (name) {
+            case "Geoq":
+            case "GaoDe":
+            case "Google":
+                zbName = "gcj02";
+                break;
+            case "Baidu":
+                zbName = "bd09";
+                break;
+            case "OSM":
+            case "TianDiTu":
+                zbName = "wgs84";
+                break;
+        }
+        return zbName;
+    }
+}
+exports.SLULeafletNetMap = SLULeafletNetMap;
+const MAPINFO = {
+    TianDiTu: {
+        Normal: {
+            Map: "//t{s}.tianditu.com/DataServer?T=vec_w&X={x}&Y={y}&L={z}&tk={key}",
+            Annotion: "//t{s}.tianditu.com/DataServer?T=cva_w&X={x}&Y={y}&L={z}&tk={key}",
+            AnnotionEn: "//t{s}.tianditu.com/DataServer?T=eva_w&X={x}&Y={y}&L={z}&tk={key}"
+        },
+        Satellite: {
+            Map: "//t{s}.tianditu.com/DataServer?T=img_w&X={x}&Y={y}&L={z}&tk={key}",
+            Annotion: "//t{s}.tianditu.com/DataServer?T=cia_w&X={x}&Y={y}&L={z}&tk={key}"
+        },
+        Terrain: {
+            Map: "//t{s}.tianditu.com/DataServer?T=ter_w&X={x}&Y={y}&L={z}&tk={key}",
+            Annotion: "//t{s}.tianditu.com/DataServer?T=cta_w&X={x}&Y={y}&L={z}&tk={key}"
+        },
+        Subdomains: ['1'],
+        key: "a9e2dd65c94fab979c9d897ff7098a4c"
+    },
+    GaoDe: {
+        Normal: {
+            Map: '//webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}'
+        },
+        Satellite: {
+            Map: '//webst0{s}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+            Annotion: '//webst0{s}.is.autonavi.com/appmaptile?style=8&x={x}&y={y}&z={z}'
+        },
+        Subdomains: ["1", "2", "3", "4"]
+    },
+    Google: {
+        Normal: {
+            Map: "//www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
+        },
+        Satellite: {
+            Map: "//www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}",
+            Annotion: "//www.google.cn/maps/vt?lyrs=y@189&gl=cn&x={x}&y={y}&z={z}"
+        },
+        Subdomains: []
+    },
+    Geoq: {
+        Normal: {
+            Map: "//map.geoq.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer/tile/{z}/{y}/{x}",
+            PurplishBlue: "//map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}",
+            Gray: "//map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetGray/MapServer/tile/{z}/{y}/{x}",
+            Warm: "//map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetWarm/MapServer/tile/{z}/{y}/{x}",
+        },
+        Theme: {
+            Hydro: "//thematic.geoq.cn/arcgis/rest/services/ThematicMaps/WorldHydroMap/MapServer/tile/{z}/{y}/{x}"
+        },
+        Subdomains: []
+    },
+    OSM: {
+        Normal: {
+            Map: "//{s}.tile.osm.org/{z}/{x}/{y}.png",
+        },
+        Subdomains: ['a', 'b', 'c']
+    },
+    Baidu: {
+        Normal: {
+            Map: '//online{s}.map.bdimg.com/onlinelabel/qt=tile&x={x}&y={y}&z={z}'
+        },
+        Satellite: {
+            Map: '//online{s}.map.bdimg.com/starpic/?qt=satepc&u=x={x}&y={y}&z={z};v=009;type=sate&fm=46&app=webearth2&v=009&udt=20231212',
+            Annotion: '//online{s}.map.bdimg.com/starpic/?qt=satepc&u=x={x}&y={y}&z={z};v=009;type=sate&fm=46&app=webearth2&v=009&udt=20231212'
+        },
+        Subdomains: '0123456789',
+        tms: true
+    }
+};
+exports.SLCRS = {
+    "Baidu": new L.Proj.CRS('EPSG:900913', '+proj=merc +a=6378206 +b=6356584.314245179 +lat_ts=0.0 +lon_0=0.0 +x_0=0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', {
+        resolutions: function () {
+            let level = 18;
+            let res = [];
+            res[0] = Math.pow(2, 18);
+            for (var i = 1; i < level; i++) {
+                res[i] = Math.pow(2, (18 - i));
+            }
+            return res;
+        }(),
+        origin: [0, 0],
+        bounds: L.bounds([20037508.342789244, 0], [0, 20037508.342789244])
+    })
+};
+//# sourceMappingURL=slu-leaflet-net-map.js.map
--- dist/sl-utils-map.js
+++ dist/sl-utils-map.js
@@ -1,6 +1,100 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MapCanvasDraw = void 0;
+exports.SLUMap = exports.MapCanvasDraw = void 0;
 var canvas_draw_1 = require("./map/canvas-draw");
 Object.defineProperty(exports, "MapCanvasDraw", { enumerable: true, get: function () { return canvas_draw_1.MapCanvasDraw; } });
+const leaflet_1 = require("leaflet");
+const AMapLoader = require("@amap/amap-jsapi-loader");
+const leaflet_2 = require("./leaflet");
+class SLUMap {
+    constructor(ele, options = {}) {
+        this.curs = Object.create(null);
+        this.createMap(ele, options);
+    }
+    showMap(names = []) {
+        const { map, curs } = this;
+        if (map && map instanceof leaflet_1.Map) {
+            let mapSource = names[0].split('.')[0];
+            let center = map.getCenter();
+            let zoom = map.getZoom();
+            map.options.crs = mapSource === 'Baidu' ? leaflet_2.SLCRS.Baidu : leaflet_1.CRS.EPSG3857;
+            map._resetView(center, zoom, true);
+            names?.forEach(name => {
+                if (curs[name])
+                    return;
+                let net = new leaflet_2.SLULeafletNetMap(name);
+                net.addTo(map);
+                curs[name] = net;
+            });
+            for (const key in curs) {
+                let name = key;
+                let flag = names.includes(name);
+                if (flag)
+                    continue;
+                curs[name].remove();
+                Reflect.deleteProperty(curs, key);
+            }
+        }
+        return this;
+    }
+    async createMap(ele, options) {
+        const { type } = options;
+        let map;
+        switch (type) {
+            case "A":
+                this.map = await this.initAmap(ele, options);
+                break;
+            default:
+                this.map = await this.initLeaflet(ele, options);
+                this.showMap([leaflet_2.SLEMap.tianDiTuNormalMap, leaflet_2.SLEMap.tianDiTuNormalAnnotion]);
+                break;
+        }
+    }
+    initLeaflet(ele, opt) {
+        const { zoom = 11, minZoom = 2, maxZoom = 20, center: [lat, lng] = [22.68471, 114.12027], dragging = true, zoomControl = false, attributionControl = false, doubleClickZoom = false, closePopupOnClick = false } = opt;
+        let param = {
+            dragging,
+            zoomControl,
+            zoom,
+            minZoom,
+            maxZoom,
+            center: (0, leaflet_1.latLng)(lat, lng),
+            attributionControl,
+            doubleClickZoom,
+            crs: leaflet_1.CRS.EPSG3857,
+            closePopupOnClick,
+        };
+        let map = new leaflet_1.Map(ele, param);
+        return Promise.resolve(map);
+    }
+    async initAmap(ele, opt) {
+        const { zoom = 11, minZoom = 2, maxZoom = 20, center: [lat, lng] = [22.68471, 114.12027], dragging = true, zoomControl = false, attributionControl = false, doubleClickZoom = false, closePopupOnClick = false, showLabel = true } = opt;
+        return AMapLoader.load({
+            "key": "87e1b1e9aa88724f69208972546fdd57",
+            "version": "1.4.15",
+            "plugins": ["Map3D"]
+        }).then(() => {
+            let map = new AMap.Map(ele, {
+                center: [lng, lat],
+                disableSocket: true,
+                viewMode: '2D',
+                mapStyle: 'amap://styles/dfd45346264e1fa2bb3b796f36cab42a',
+                skyColor: "#A3CCFF",
+                lang: 'zh_cn',
+                labelzIndex: 130,
+                pitch: 40,
+                zoom: zoom,
+                zooms: [minZoom, maxZoom],
+                dragEnable: dragging,
+                doubleClickZoom: doubleClickZoom,
+                keyboardEnable: false,
+                isHotspot: false,
+                showLabel,
+                layers: [],
+            });
+            return map;
+        });
+    }
+}
+exports.SLUMap = SLUMap;
 //# sourceMappingURL=sl-utils-map.js.map
